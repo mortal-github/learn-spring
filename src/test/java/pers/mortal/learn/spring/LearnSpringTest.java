884bacc2679c24c3c139f3ccbbdef98ab6d68fe5
mortal-github <13710503031@139.com>
2021-03-01 15:50:23 +0800

    Initial project and add spring dependencies.

---
+++ src/test/java/pers/mortal/learn/spring/LearnSpringTest.java
@@ -0,0 +1,20 @@
+package pers.mortal.learn.spring;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class LearnSpringTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
