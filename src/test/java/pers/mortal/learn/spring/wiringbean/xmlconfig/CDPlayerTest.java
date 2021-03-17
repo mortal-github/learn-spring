5cfece6a6cb6fe6ac182d7cbcc73656582e330b9
mortal-github <13710503031@139.com>
2021-03-17 17:51:36 +0800

    Fix: SpringJunitText

--- src/test/java/pers/mortal/learn/spring/wiringbean/xmlconfig/CDPlayerTest.java
+++ src/test/java/pers/mortal/learn/spring/wiringbean/xmlconfig/CDPlayerTest.java
@@ -2,22 +2,25 @@ package pers.mortal.learn.spring.wiringbean.xmlconfig;
 
 import org.junit.Assert;
 import org.junit.Test;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:/META-INF/wiring_bean/wiring_bean.xml"})
 public class CDPlayerTest {
-    private static ClassPathXmlApplicationContext context;
-    private static CompactDisc cd;
-    private static CDPlayer player;
-    private static BlankDisc bcd;
+    @Autowired
+    private SgtPeppers cd;
+    
+    @Autowired
+    private CDPlayer player;
+
+    @Autowired
+    private BlankDisc bcd;
 
     @Test
     public void play(){
-        context = new ClassPathXmlApplicationContext("/META-INF/wiring_bean/wiring_bean.xml");
-        cd = context.getBean(SgtPeppers.class);
-        player = context.getBean(CDPlayer.class);
-        bcd = context.getBean(BlankDisc.class);
-
-        Assert.assertNotNull(context);
         Assert.assertNotNull(cd);
         Assert.assertNotNull(player);
         Assert.assertNotNull(bcd);
